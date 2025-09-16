710c2e1d15647474ec98bd559de2f3c7ef509688
CthulhuTheGod <oxot999@mail.ru>
2025-09-16 12:13:05 +0300

    M

--- py_labs/lab2.1.py
+++
@@ -1,6 +0,0 @@
-my_range = range(1, 11)
-for i in my_range:
-    my_list = list()
-    for j in my_range:
-        my_list.append(i*j)
-    print(my_list)
---
+++ py_practice/kek.py
@@ -0,0 +1,3 @@
+a = 5
+b = bin(a)
+print(int("101", 2))
